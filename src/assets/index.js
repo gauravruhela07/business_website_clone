32e6dc3b81f4facab7b53c10d2e544f5aa615b96
Gaurav Ruhela <gauravruhela@MacBook-Pro-7.local>
2023-07-30 14:49:19 +0400

    client component

--- src/assets/index.js
+++ src/assets/index.js
@@ -15,6 +15,12 @@ import mary from './mary.png';
 import fb from './fb.png';
 import twitter from './twitter.png';
 import insta from './insta.png';
+import client_person from './client_person.png';
+import client1 from './client1.jpeg';
+import client2 from './client2.jpeg';
+import client3 from './client3.jpeg';
+import client4 from './client4.jpeg';
+import client5 from './client5.jpeg';
 
 export {
     logo, 
@@ -28,5 +34,7 @@ export {
     storage,
     www,
     bob, mary,
-    fb, twitter, insta
+    fb, twitter, insta,
+    client_person,
+    client1, client2, client3, client4, client5
 };
--- src/components/Client.jsx
+++ src/components/Client.jsx
@@ -1,8 +1,46 @@
 import React from 'react'
 
+import { client_person, client1, client2, client3, client4, client5 } from '../assets'
+
 const Client = () => {
+
+    const clientList = [client1, client2, client3, client4, client5];
+
   return (
-    <div>Client</div>
+    <div className="w-full h-[800px] flex flex-col bg-white">        
+        <div className="flex flex-row mx-[10%] justify-center py-5">
+            <div className="h-[455px] w-[570px] mr-7">
+                <div className="flex flex-col py-10 my-10 ml-20">
+                    <p className="flex justify-end text-4xl font-bold">Out Client</p>
+                    <p className="flex justify-end mt-5">
+                        A joint study by BCG and the World Federation of People 
+                        <br/> Management Associations, based on a global survey,
+                        <br/>focuses on three priority areas for HR action. Ut enim 
+                        <br/>ad minim veniam, quis nostrud exercitation ullamco laboris 
+                        <br/>nisi ut aliquip ex ea commodo consequat. Duis aute irure 
+                        <br/>dolor in reprehenderit in voluptate velit esse cillum dolore 
+                        <br/>eu fugiat nulla pariatur.
+                    </p>
+                    <div className='flex justify-end h-[45px]'>
+                        <p className='mt-5'>Images by <a className="underline">FreePik</a></p>
+                    </div>
+                </div>
+            </div>
+            <div className="h-[455px] w-[570px] ml-7 relative">
+                <img 
+                    src={client_person}
+                    alt="client person"
+                    className="h-full p-7 object-contain z-10 absolute"
+                />
+                <div className="w-[299px] h-[251px] bg-[#654aff] mt-20 ml-20 absolute" />                
+            </div>
+        </div>
+        <div className='flex justify-center mt-10'>
+            {clientList.map((client) => (
+                <img src={client} className='mx-10 py-5'/>
+            ))}            
+        </div>
+    </div>
   )
 }
 
